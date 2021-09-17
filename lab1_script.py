c19580b588adab57956bbaa916a08ec87b4b1793
Zijie Tan <32871093+ZjTan4@users.noreply.github.com>
2021-09-17 12:00:53 -0700

    up to step 5

---
+++ lab1_script.py
@@ -0,0 +1,11 @@
+import requests
+
+# Question 2 % 3
+print(requests.__version__)
+# Step 5
+googleHomePage = requests.get("http://www.google.com/")
+#print(googleHomePage.status_code)
+#print(googleHomePage.text)
+
+
+raw_link = "https://raw.githubusercontent.com/ZjTan4/cmput404lab1/main/lab1_script.py"
